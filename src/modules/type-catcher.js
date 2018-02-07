bf2702b0ded1ee89fc6125cbc65d1fb006011355
hmayak.hambardzumyan <hmayak.hambardzumyan@betconstruct.com>
2018-02-07 16:07:14 +0400

    Fix case ingnore config git.

--- src/modules/type-catcher.js
+++ src/modules/type-catcher.js
@@ -11,3 +11,35 @@ const typeCatcher = function(variable) {
 }
 
 export { typeCatcher }
+
+// init() {
+// 	Object.defineProperty(this.env.Array.prototype, [this.libName], {
+// 		value: 'Array',
+// 		writable: false,
+// 		configurable: false,
+// 		enumerable: true
+// 	});
+//
+// 	Object.defineProperty(this.env.Number.prototype, [this.libName], {
+// 		configurable: false,
+// 		enumerable: true,
+// 		get: function() {
+// 			return parseInt(this.toString()) === parseInt(this.toString()) ? 'Number' : this.toString();
+// 		}
+// 	});
+//
+// 	Object.defineProperty(this.env.String.prototype, [this.libName], {
+// 		value: 'String',
+// 		writable: false,
+// 		configurable: false,
+// 		enumerable: true
+// 	});
+//
+// 	Object.defineProperty(this.env.Object.prototype, [this.libName], {
+// 		configurable: false,
+// 		enumerable: true,
+// 		get: function() {
+// 			return this.constructor.name;
+// 		}
+// 	});
+// }
